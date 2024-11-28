62d264a3dea5ff761fd17aff4fa245ab68196f22
Koustav Chowdhury <kc99.kol@gmail.com>
2024-11-28 23:25:47 +0530

    challenge 3b: Multi-Node Broadcast

--- maelstrom-broadcast/main.go
+++ maelstrom-broadcast/main.go
@@ -20,17 +20,51 @@ func main() {
 			return err
 		}
 
-		// Update the message type to return back.
-		body["type"] = "broadcast_ok"
+		receiver := msg.Src
+		log.Println("broadcast message received from: ", receiver)
+		log.Println("broadcast message received with body: ", body)
 
-		broadcast_message := body["message"]
-		messages = append(messages, broadcast_message)
+		bcast_message := body["message"]
+		messages = append(messages, bcast_message)
 
 		// delete the message from the body
 		delete(body, "message")
 
+		// we need to broadcast the message to all the neighbors
+		// we can chunk up the message and send it to each neighbor in batches
+
+		// Get the list for this node
+		go func() {
+			for _, neighbor := range neighbor_list {
+				// do not send message back to the sender
+				if neighbor == receiver {
+					continue
+				}
+				log.Printf("neighbor: %s", neighbor)
+				log.Printf("message: %v", bcast_message)
+
+				broadcast_message := map[string]any{
+					"type":    "broadcast",
+					"message": bcast_message,
+				}
+
+				log.Printf("broadcast message: %v", broadcast_message)
+
+				// Send the message to the neighbor
+				if err := n.Send(neighbor, broadcast_message); err != nil {
+					log.Printf("failed to send message to %s: %v", neighbor, err)
+				}
+			}
+		}()
+
+		// Update the message type to return back.
+		body["type"] = "broadcast_ok"
 		// Echo the original message back with the updated message type.
 		return n.Reply(msg, body)
+	})
+
+	n.Handle("broadcast_ok", func(msg maelstrom.Message) error {
+		return nil
 	})
 
 	n.Handle("read", func(msg maelstrom.Message) error {
